691d956ebc208809ac06019d3f38b535e8624ea9
devogs <billy.bob@devogs.xyz>
2025-10-27 18:27:36 +0100

    CHORE: Remove all flake8 occurences

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -8,14 +8,11 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # Lettings App URLs (using include for modularity)
-    path('lettings/', include('lettings.urls')), 
+    path('lettings/', include('lettings.urls')),
 
     # Profiles App URLs (using include for modularity)
     path('profiles/', include('profiles.urls')),
-    
+
     # Admin
     path('admin/', admin.site.urls),
 ]
-
-# Note: The original paths for lettings_index, letting, profiles_index, and profile
-# have been REMOVED from this file.
