ff4dfd8dfdefad2473b1bfa455104bfce91cc131
devogs <billy.bob@devogs.xyz>
2025-11-14 17:13:08 +0100

    TEST: Modification du Title

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oc_lettings_site',
+    version='2.0',
+    packages=find_packages(),
+    description='Projet OC Lettings FR (Devogs Refactoring)',
+    author='Mathieu P.',
+    install_requires=[],
+)
